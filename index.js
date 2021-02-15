5b218ba5d464f61256f319c369f2eb2ab643cad8
BuildTools <ctre.gautier@gmail.com>
2021-02-15 20:25:17 +0100

    edit index :wrench:

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 import fastify from 'fastify';
-// see axios doc on how to use it
 import axios from 'axios';
 
 const app = fastify({ logger: true });
 
 app.get('/', async (req, res) => {
   return {
-    message: `Welcome to Node Babel with ${
-      req.body?.testValue ?? 'no testValue'
-    }`,
+    catfacts: getCatFacts(),
+    foxes: getFoxDay(),
+    day: getDayOfCountry()
   };
 });
 
-// Run the server!
+const getCatFacts = async(id) =>{
+  axios.get('https://cat-fact.herokuapp.com/facts/{id}').then(res => {});
+};
+
+const getFoxDay = async(id) =>{
+  axios.get('https://randomfox.ca/floof/').then(res => {});
+};
+
+const getDayOfCountry = async(id) =>{
+  axios.get('https://randomfox.ca/floof/').then(res => {});
+};
+
 const start = async () => {
   try {
     await app.listen(5000);
+    getCatFacts(1);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
