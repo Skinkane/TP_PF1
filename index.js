4da2c3b4832febfcae79c4b414cef156a41b52a4
Skinkane <ctre.gautier@gmail.com>
2021-02-16 18:31:46 +0100

    add api calls and start editing format :wrench:

--- index.js
+++ index.js
@@ -4,29 +4,46 @@ import axios from 'axios';
 const app = fastify({ logger: true });
 
 app.get('/', async (req, res) => {
-  return {
-    catfacts: getCatFacts(),
-    foxes: getFoxDay(),
-    day: getDayOfCountry()
-  };
+  return getAll();
 });
 
-const getCatFacts = async(id) =>{
-  axios.get('https://cat-fact.herokuapp.com/facts/{id}').then(res => {});
+
+const getCatFacts = () =>{
+  let facts = [];
+  return new Promise(resolve => {axios.get('https://cat-fact.herokuapp.com/facts').then(res => {
+    let facts = [];
+    let i = 0;
+    while (i < 3) {
+      facts.push(res.data[i].text);
+      i++;
+    };
+    resolve(facts);
+  });});
 };
 
-const getFoxDay = async(id) =>{
-  axios.get('https://randomfox.ca/floof/').then(res => {});
+const getFoxDay = () =>{
+  return new Promise(resolve => {
+    axios.get('https://randomfox.ca/floof/').then(res => {resolve(res.data.image)});
+  });
 };
 
-const getDayOfCountry = async(id) =>{
-  axios.get('https://randomfox.ca/floof/').then(res => {});
+const getDayOfCountry = () => {
+  return new Promise(resolve => {
+    axios.get('https://date.nager.at/api/v2/publicholidays/2019/FR').then(res => {resolve(res.data)});
+  });
 };
+
+const getAll = () => {
+  let promises = Promise.all([getCatFacts(),getFoxDay(),getDayOfCountry()]);
+  console.log([promises[1]]);
+  let data = promises
+  return data;
+};
+
 
 const start = async () => {
   try {
     await app.listen(5000);
-    getCatFacts(1);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
